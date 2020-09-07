922d93de9dfb6f532e6252e82e788e28bf2b9bf8
reteid <diets1@gmail.com>
2020-09-07 11:09:01 +0100

    Update Program.cs
    httpResponseMessage.StatusCode is 400 (bad request ) unless you use https and set the security protocol as per my change

--- StorageRestApiAuth/Program.cs
+++ StorageRestApiAuth/Program.cs
@@ -31,10 +31,12 @@
         /// </summary>
         private static async Task ListContainersAsyncREST(string storageAccountName, string storageAccountKey, CancellationToken cancellationToken)
         {
-
+            //Add security protocol for HTTPS
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+            
             // Construct the URI. This will look like this:
             //   https://myaccount.blob.core.windows.net/resource
-            String uri = string.Format("http://{0}.blob.core.windows.net?comp=list", storageAccountName);
+            String uri = string.Format("https://{0}.blob.core.windows.net?comp=list", storageAccountName);
 
             // Set this to whatever payload you desire. Ours is null because 
             //   we're not passing anything in.
